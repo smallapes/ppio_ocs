f53d161f15bf920d8102300a3d1e8ebba2e859c7
maoxianren <maoxianren@example.com>
2023-12-07 10:04:37 +0000

    version: 2.1

---
+++ start.sh
@@ -0,0 +1,7 @@
+export OPENAI_API_KEY=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
+export ZHIPUAI_API_KEY=b47299711bd194e731ec4323aa64c840.7XxCW8Iag0xXAFFW
+export SERPAPI_API_KEY=5f0619a1231f66569a4f7dce9adbf8001b3a21cb6face3c3ac35d6c3dc5981dd
+
+
+cd /home/aitest2/maoxianren/langchain-chatchat
+nohup /home/aitest2/maoxianren/langchain-chatchat/venv/bin/python startup.py -a >> log.txt &
